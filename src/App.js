ed18e1523a897723482c0ab068783438061e7ceb
jayaramkrish <jayaramakrishna.vittanala@gmail.com>
2022-10-05 22:07:21 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           chi po ra bangaram 
+           attitude bolthey 
         </p>
         <a
           className="App-link"
