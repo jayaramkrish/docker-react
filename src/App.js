d9a6db4cde009572a475386c27b70e682e40ab78
jayaramkrish <jayaramakrishna.vittanala@gmail.com>
2022-10-06 15:21:52 +0530

    psycho samba commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           attitude bolthey 
+           attitude bolthey ra psychosamba 
         </p>
         <a
           className="App-link"
