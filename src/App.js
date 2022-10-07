baf0ce80c68c5d226e60da5c1a4ac80335292452
jayaramkrish <jayaramakrishna.vittanala@gmail.com>
2022-10-07 23:01:38 +0530

    hehe

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           First Deployment with AWS Elastic beanstalk
+           First Deployment with AWS  Elastic beanstalk
         </p>
         <a
           className="App-link"
